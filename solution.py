c68290af9a52ff9c86ea39fd29dc6d4445bac670
drapkinamaria <drapkinamaria02@gmail.com>
2023-04-14 01:32:05 +0500

    try2.1

--- solution.py
+++ solution.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+from scipy.stats import anderson_ksamp, ks_2samp, cramervonmises_2samp, mannwhitneyu
 
-
-chat_id = 760034497 # Ваш chat ID, не меняйте название переменной
+chat_id = 532761772 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return anderson_ksamp([x, y]).pvalue <= 0.01 # Ваш ответ, True или False
+   return cramervonmises_2samp(x, y).pvalue < 0.05 # Ваш ответ, True или False

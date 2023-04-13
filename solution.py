863574487d2cc2dcf5cf42b2de0b912a5122f9d9
drapkinamaria <drapkinamaria02@gmail.com>
2023-04-14 01:47:20 +0500

    try3

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import anderson_ksamp, ks_2samp, cramervonmises_2samp, mannwhitneyu
+from hyppo.ksample import MMD
 
 chat_id = 532761772 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +9,5 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-   return cramervonmises_2samp(x, y).pvalue < 0.05 # Ваш ответ, True или False
+   pval = MMD(compute_kernel="rbf", gamma=1).test(x, y)[1]
+   return pval < 0.05
